99bf3f63a23d5423c84546ca54aeece71f40e90e
Joseph Smith <4484836+jsmmth@users.noreply.github.com>
2025-11-06 19:36:15 +0000

    chore: attempt another fix

--- Sources/MorphModalKit/PossiblePassThroughView.swift
+++ Sources/MorphModalKit/PossiblePassThroughView.swift
@@ -2,22 +2,28 @@ import UIKit
 
 final class PossiblePassThroughView: UIView {
     weak var modalVC: ModalViewController?
-    
+
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard let modalVC = modalVC else {
             return super.hitTest(point, with: event)
         }
-    
-        guard modalVC.passThroughTouches else {
-            return super.hitTest(point, with: event)
-        }
-        
-        for container in modalVC.containerStack.reversed() {
-            if container.wrapper.frame.contains(point) {
-                return super.hitTest(point, with: event)
+
+        if modalVC.passThroughTouches {
+            for container in modalVC.containerStack.reversed() {
+                let convertedPoint = convert(point, to: container.wrapper)
+                if container.wrapper.bounds.contains(convertedPoint) {
+                    // Touch is on a modal, handle it normally
+                    if let hitView = container.wrapper.hitTest(
+                        convertedPoint,
+                        with: event
+                    ) {
+                        return hitView
+                    }
+                }
             }
+            return nil
         }
 
-        return nil
+        return super.hitTest(point, with: event)
     }
 }
